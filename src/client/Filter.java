e7afb20d4d85d981fd777ffcda767d93fbbe7d3d
Usuario <Usuario@Usuario-PC>
2012-11-20 12:31:07 -0300

    Filtros

--- src/client/Filter.java
+++ src/client/Filter.java
@@ -15,6 +15,56 @@ public class Filter {
 		action.act(e,c);}
 	}
 
+
+
+public boolean isExclusive() {
+	return exclusive;
+}
+
+
+public void setExclusive(boolean exclusive) {
+	this.exclusive = exclusive;
+}
+
+
+public Rule getRule() {
+	return rule;
+}
+
+
+public void setRule(Rule rule) {
+	this.rule = rule;
+}
+
+
+public Field getField() {
+	return field;
+}
+
+
+public void setField(Field field) {
+	this.field = field;
+}
+
+
+public Action getAction() {
+	return action;
+}
+
+
+public void setAction(Action action) {
+	this.action = action;
+}
+
+    
+
+public Filter(boolean exc,Rule r,Field f,Action a){
+	this.setExclusive(exc);
+	this.setRule(r);
+	this.setField(f);
+	this.setAction(a);
+}
+
 }
 
 	
