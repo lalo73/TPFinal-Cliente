922af50e6593277d80add0ec6ec78695cbe66729
matlock <matlock@matlock-Lenovo-G470>
2012-11-24 19:46:00 -0300

    Agregada la clase Email y Folder.. Se han completados las interfaces correspondientes a las clases..

--- src/client/IEmail.java
+++ src/client/IEmail.java
@@ -1,5 +1,13 @@
 package client;
 
+import java.util.ArrayList;
+
 public interface IEmail {
+	 public boolean isReaded();
+	 public ArrayList<ClientEmailAttachment> getAttachment();
+	 public String getBody();
+	 public ClientEmailHead getHead();
+	 
+	 
 
 }
