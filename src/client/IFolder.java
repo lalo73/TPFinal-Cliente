8b70c0a8ef664255db07e143c0edf8614618279a
matlock <matlock@matlock-Lenovo-G470>
2012-11-26 15:36:46 -0300

    Arreglado el error del Merge

--- src/client/IFolder.java
+++ src/client/IFolder.java
@@ -1,7 +1,4 @@
 package client;
-
-import java.util.ArrayList;
-
 import client.IEmail;
 
 
@@ -9,21 +6,13 @@ import client.IEmail;
 	public interface IFolder {
 		
 		public void add(IEmail e);
-	<<<<<<< HEAD
 		public void removeEmail(IEmail e);
-	=======
-		public void remove(IEmail e);
-
-	>>>>>>> branch 'master' of https://github.com/lalo73/TPFinal-Cliente.git
 		public boolean includes(IEmail e);
 		public void clearList();
 		public IEmail getEmail(IEmail e) throws Exception;
 		public void setFolderName(String folderName);
-	<<<<<<< HEAD
-	=======
-		public void setEmail(ArrayList<IEmail> email);
-
-	>>>>>>> branch 'master' of https://github.com/lalo73/TPFinal-Cliente.git
+		
+		
 	}
 
-}
+
